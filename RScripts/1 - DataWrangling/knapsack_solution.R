a5b477c35c40c5c4acb1e6e9c3ebc826878d2cb0
monty09 <sososhibby@yahoo.com>
2018-11-02 15:40:43 -0400

    Add files via upload

--- RScripts/1 - DataWrangling/knapsack_solution.R
+++ RScripts/1 - DataWrangling/knapsack_solution.R
@@ -1,109 +1,22 @@
-rm(list=ls())
-functions <- list()
-project_location <- paste0("/AnalysisProjects/DraftKings/")
-working_directory <- paste0(file.path(Sys.getenv("USERPROFILE"),"Documents"),project_location)
-setwd(working_directory)
-print(getwd())
-
-functions$packages <- function(){
-  Sys.setenv(TZ = "America/New_York")
-  #Sys.setenv(JAVA_HOME='C:\\Program Files (x86)\\Java\\jre1.8.0_181')
-  Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_181')
-  
-  
-  
-  package_names <- c('caret','mlbench','glmnet','h2o','modelr','tidyverse','rJava','xlsx',"readxl","devtools", "futile.logger", "DBI", "RMySQL", "uuid", "readr", "caret", "gbm", "rpart", "randomForest", "stringdist", "tm", "curl", "stringr", "jsonlite", "purrr", "dplyr", "tidyr", "fastmatch")
-  ok = sapply(package_names,
-              FUN = function (package_name) {
-                suppressMessages(require(package_name, character.only=TRUE, quietly = T, warn.conflicts = F))
-              })
-  
-}
-functions$packages()
-
-dk.14 <- read_csv(file = './Data/dk_csv/ByYear/dk_2014.csv'
-                  ,col_types = cols(.default = "c"))
-
-dk.14 <- dk.14 %>% mutate_at(.vars = vars(Week,DK.points,DK.salary),.funs = funs(as.numeric)) 
-
-#Get rid of non defense 0 point players
-dk.14 <- rbind(dk.14 %>% filter(DK.points==0,Pos=='Def'),dk.14 %>% filter(DK.points!=0))
-#Keep highest salary, then highest point qb of the week
-dk.14.qb <- dk.14 %>% filter(Pos=='QB') %>% group_by(Team,Week,Pos) %>% mutate(max_qb = max(DK.salary)) %>% mutate(max_qb_p = max(DK.points)) %>% ungroup() %>% filter(DK.salary == max_qb) %>% filter(DK.points == max_qb_p)
-dk.14 <- rbind(dk.14 %>% filter(Pos!='QB'),dk.14.qb %>% select(-max_qb,-max_qb_p))
-rm(dk.14.qb)
-
-###For a given week, return a team's points and salary by position
-functions$player_week_points_salary_pos <- function(weekly_dataframe,week_num){
-  pts_week_name <- paste0("Week.",week_num,".Pts")
-  salary_week_name <- paste0("Week.",week_num,".Salary")
-  
-  return_df <-
-    weekly_dataframe %>% 
-    filter(Week == week_num) %>% 
-    select(Name,GID,Pos,Team,DK.points,DK.salary) %>%
-    mutate(!!pts_week_name := DK.points) %>%
-    mutate(!!salary_week_name := DK.salary) %>%
-    distinct_('Name','GID','Pos','Team',pts_week_name,salary_week_name) %>%
-    ungroup()
-  
-  return(return_df)
-}
-
-###Add in home and away averages to dataframes
-functions$player_home_away_averages <- function(weekly_dataframe){
-  player_home_away_avgs <-
-    weekly_dataframe %>%
-    group_by(Team,Pos,Week,GID,Name) %>%
-    mutate(sum_pts = sum(DK.points)) %>%
-    mutate(sum_sals = sum(DK.salary)) %>%
-    ungroup() %>%
-    group_by(GID,Name,Team,Pos,h.a) %>%
-    mutate(avg_points = round(mean(sum_pts),2)) %>%
-    mutate(avg_salary = round(mean(sum_sals),2)) %>%
-    distinct(GID,Name,Team,Pos,h.a,avg_points,avg_salary) %>%
-    tidyr::unite(all_avg,avg_points,avg_salary) %>%
-    spread(h.a,all_avg) %>%
-    separate(col = "a",into = c("away_avg_pts","away_avg_salary"),sep = "_") %>%
-    separate(col = "h",into = c("home_avg_pts","home_avg_salary"),sep = "_")
-  return(player_home_away_avgs)
-}
-
-###This will transform into the Player's points and salary by week
-functions$player_salary_points <- function(weekly_dataframe){
-  
-  dataframes <- lapply(sort(unique(weekly_dataframe$Week)),function(week){
-    functions$player_week_points_salary_pos(weekly_dataframe,week)
-  })
-  
-  rdf <- plyr::join_all(dfs = dataframes,by = c('Name','GID','Pos','Team'),type='full')
-  player_home_away_avgs <- functions$player_home_away_averages(weekly_dataframe)
-  
-  all_players <- 
-    weekly_dataframe %>%
-    distinct(Name,GID,Pos,Team) %>%
-    dplyr::left_join(player_home_away_avgs,by=c('Name'='Name','GID'='GID','Pos'='Pos','Team'='Team')) %>%
-    dplyr::left_join(rdf,by=c('Name'='Name','GID'='GID','Pos'='Pos','Team'='Team'))
-  
-  
-  return(all_players)
-}
 
 
-player_byweek_dataframe_2014 <- functions$player_salary_points(dk.14) 
-
-week1_player_df <- 
-  player_byweek_dataframe_2014 %>%
-  select(GID,Name,Pos,Team,Week.1.Pts,Week.1.Salary) %>%
-  filter(!is.na(Week.1.Salary),!is.na(Week.1.Pts))
-
+library(lpSolveAPI)
+library(lpSolve)
+library(FLSSS)
+library(adagio)
+library(ompr)
+library(ompr.roi)
+library(ROI)
+library(ROI.plugin.glpk)
 
 library(lpSolve)
 find_teams <- function(train, cap = 50000, constraint = "none", league = "DraftKings", setplayers = NULL,removeplayers = NULL, removeteams = NULL) {
-  
-  colnames(train) <- c("GID", "Name", "Pos", "Team", "Pts", "Salary")
+
+  colnames(train) <- c("GID", "Name", "Pos", "Team","Week", "Pts", "Salary")
   
   ## set constraints to use
+  train$Pts <- as.numeric(train$Pts)
+  train$Salary <- as.numeric(train$Salary)
   
   qb <- ifelse(train$Pos == "QB", 1, 0)
   wr <- ifelse(train$Pos == "WR", 1, 0)
@@ -124,9 +37,12 @@ find_teams <- function(train, cap = 50000, constraint = "none", league = "DraftK
 
   dk_total <- qb + wr + rb + te + def
   add.constraint(lpfantasy, qb, "=", 1)
-  add.constraint(lpfantasy, wr, "<=", 4)
   add.constraint(lpfantasy, rb, "<=", 3)
+  add.constraint(lpfantasy, rb, ">=", 2)
+  add.constraint(lpfantasy, wr, "<=", 4)
+  add.constraint(lpfantasy, wr, ">=", 3)
   add.constraint(lpfantasy, te, "<=", 2)
+  add.constraint(lpfantasy, te, ">=", 1)
   add.constraint(lpfantasy, def, "=", 1)
   add.constraint(lpfantasy, dk_total, "=", 9)
   
@@ -182,34 +98,190 @@ find_teams <- function(train, cap = 50000, constraint = "none", league = "DraftK
   team
 }
 
-train <- week1_player_df
-
-removeplayers = NULL
-teams <- vector("list")
-for(i in 1:5){
-  print(i)
-  team <- paste0("Team",i)
-  result <- find_teams(train,removeplayers = removeplayers)
-  removeplayers <- rbind(removeplayers,train %>% filter(GID %in% result$GID))
-  result$team_num <- i
-  teams[[team]] <- result
+top_teams <- function(train, num_top = 5) {
+  result <- find_teams(train)
+  restrict <- as.matrix(rep(0, nrow(train)))
+  restrict[match(result$GID, train$GID), 1] <- 1
+  j <- 1
   
+  while(j < num_top) {
+    resultnew <- find_teams(train, removeteams = restrict)
+    restrict <- cbind(restrict, rep(0, nrow(restrict)))
+    restrict[match(resultnew$GID, train$GID), j] <- 1
+    result <- rbind(result, resultnew)
+    j <- j + 1
+  }
+  
+  TeamNumber <- rep(1:num_top, each = 9)
+  result <- cbind.data.frame(result, TeamNumber)
+  result
+} 
+
+independent_teams <- function(train){
+  removeplayers = NULL
+  teams <- vector("list")
+  for(i in 1:5){
+    #print(i)
+    team <- paste0("Team",i)
+    result <- find_teams(train,removeplayers = removeplayers)
+    removeplayers <- rbind(removeplayers,train %>% filter(GID %in% result$GID))
+    result$TeamNumber <- i
+    teams[[team]] <- result
+  }
+  all_teams <- teams %>% bind_rows()
+  return(all_teams)
 }
 
-all_teams <- teams %>% bind_rows()
+
+
+
+dk_files <- list.files(path = 'C:/Users/MonticT/Documents/AnalysisProjects/DraftKings/Data/dk_csv/ByYear/',full.names = T)
+dk.14 <- read_csv(file = dk_files[1],col_types = cols(.default = "c"))
+dk.15 <- read_csv(file = dk_files[2],col_types = cols(.default = "c"))
+dk.16 <- read_csv(file = dk_files[3],col_types = cols(.default = "c"))
+dk.17 <- read_csv(file = dk_files[4],col_types = cols(.default = "c"))
+
+colnames(train) <- c("GID", "Name", "Pos", "Team","Week", "Pts", "Salary")
+dk.14 <- dk.14 %>% select(GID,Name,Pos,Team,Week,DK.points,DK.salary) %>% rename(Pts="DK.points",Salary="DK.salary")
+dk.15 <- dk.15 %>% select(GID,Name,Pos,Team,Week,DK.points,DK.salary) %>% rename(Pts="DK.points",Salary="DK.salary")
+dk.16 <- dk.16 %>% select(GID,Name,Pos,Team,Week,DK.points,DK.salary) %>% rename(Pts="DK.points",Salary="DK.salary")
+dk.17 <- dk.17 %>% select(GID,Name,Pos,Team,Week,DK.points,DK.salary) %>% rename(Pts="DK.points",Salary="DK.salary")
+
+train <- dk.14 %>% filter(Week==1)
+top_teams(train,5)
+independent_teams(train)
+
+
+best14 <- lapply(unique(dk.14$Week),function(week){
+  y <- dk.14 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- top_teams(train=y,5)
+  x$year <- 2014
+  x
+}) %>% bind_rows()
+
+best15 <- lapply(unique(dk.15$Week),function(week){
+  y <- dk.15 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- top_teams(train=y,5)
+  x$year <- 2015
+  x
+}) %>% bind_rows()
+
+best16 <- lapply(unique(dk.16$Week),function(week){
+  y <- dk.16 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- top_teams(train=y,5)
+  x$year <- 2016
+  x
+}) %>% bind_rows()
+
+best17 <- lapply(unique(dk.17$Week),function(week){
+  y <- dk.17 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- top_teams(train=y,5)
+  x$year <- 2017
+  x
+}) %>% bind_rows()
+
+best_all <- rbind(best14,best15,best16,best17)
+
+
+position_breakdown <- function(winner_dataframe,year,with_team_number = F){
+  if(with_team_number){
+    best_how <- 
+      winner_dataframe %>% 
+      select(Pos,Week,year,TeamNumber) %>%
+      group_by(Week,year,TeamNumber) %>%
+      add_count(Pos) %>%
+      ungroup() %>%
+      distinct(Pos,Week,year,TeamNumber,n) %>%
+      filter(Pos %in% c('RB','WR','TE'))
+    
+    num_weeks <- best_how %>% distinct(Week,year,TeamNumber) %>% nrow()
+  }
+  
+  if(!with_team_number){
+    best_how <- 
+      winner_dataframe %>% 
+      select(Pos,Week,year,TeamNumber) %>%
+      group_by(Week,year,TeamNumber) %>%
+      add_count(Pos) %>%
+      ungroup() %>%
+      distinct(Pos,Week,year,TeamNumber,n) %>%
+      filter(Pos %in% c('RB','WR','TE'))
+    
+    num_weeks <- best_how %>% distinct(Week,year) %>% nrow()  
+  }
+  
+  best_rb_count <- best_how %>% filter(Pos=='RB',n==3) %>% nrow()
+  best_wr_count <- best_how %>% filter(Pos=='WR',n==4) %>% nrow()
+  best_te_count <- best_how %>% filter(Pos=='TE',n==2) %>% nrow()
+  
+  rb_flex_percent <- best_rb_count / num_weeks * 100
+  wr_flex_percent <- best_wr_count / num_weeks * 100
+  te_flex_percent <- best_te_count / num_weeks * 100
+  
+  print(paste0("For Year(s): ",year," 'Winning Lineups have Flex position of' "," RB Flex is: ",round(rb_flex_percent,2),"% WR Flex is: ",round(wr_flex_percent,2),"% TE Flex is: ",round(te_flex_percent,2),"%"))
+
+}
+
+all_best5 <- function(){
+  print("DraftKings Winning Lineup Quick Breakdown")
+  print("Using 5 Best Teams Per Week: ")
+  position_breakdown(best_all,'2014 to 2017',T)
+  position_breakdown(best17,'2017',T)
+  position_breakdown(best16,'2016',T)
+  position_breakdown(best15,'2015',T)
+  position_breakdown(best14,'2014',T)
+}
+all_best5()
 
 
 
 
 
-library(lpSolveAPI)
-library(lpSolve)
-library(FLSSS)
-library(adagio)
-library(ompr)
-library(ompr.roi)
-library(ROI)
-library(ROI.plugin.glpk)
+best14 <- lapply(unique(dk.14$Week),function(week){
+  y <- dk.14 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- independent_teams(train=y)
+  x$year <- 2014
+  x
+}) %>% bind_rows()
+
+best15 <- lapply(unique(dk.15$Week),function(week){
+  y <- dk.15 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- independent_teams(train=y)
+  x$year <- 2015
+  x
+}) %>% bind_rows()
+
+best16 <- lapply(unique(dk.16$Week),function(week){
+  y <- dk.16 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- independent_teams(train=y)
+  x$year <- 2016
+  x
+}) %>% bind_rows()
+
+best17 <- lapply(unique(dk.17$Week),function(week){
+  y <- dk.17 %>% filter(Week==week,!is.na(Pts),Salary > 0)
+  x <- independent_teams(train=y)
+  x$year <- 2017
+  x
+}) %>% bind_rows()
+
+best_all <- rbind(best14,best15,best16,best17)
+
+all_best_ind5 <- function(){
+  print("DraftKings Winning Lineup Quick Breakdown")
+  print("Using 5 Best Independent Teams Per Week: ")
+  position_breakdown(best_all,'2014 to 2017',T)
+  position_breakdown(best17,'2017',T)
+  position_breakdown(best16,'2016',T)
+  position_breakdown(best15,'2015',T)
+  position_breakdown(best14,'2014',T)
+}
+all_best_ind5()
+
+
+
+
+
 
 
 
